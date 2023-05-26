53d0d08bb7ae6393845964b85480a76f26170853
JerichoValerio <jerichotvalerio@gmail.com>
2023-05-26 15:21:47 -0700

    waiting for push

---
+++ Week 4 (Forms)/employeeManagement/src/Components/EmployeeForm/EmployeeForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function EmployeeForm{
+
+  return (
+    <form>
+      <div ClassName={styles.formControl}>
+        <label htmlFor=""</label>
+        <input type="date" placeholder='cscscss' />
+      /<div>
+  )
+}
---
+++ Week 4 (Forms)/forms/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import UncontrolledForm from './components/UncontrolledForm/UncontrolledForm'
+import ControlledForm from './components/controlledForms/ControlledForm'
+
+function App() {
+
+  return (
+      <div className='main-container'>
+        <h1>CONTROLLED FORM</h1>
+        <ControlledForm />
+        <h1>UNCONTROLLED FORM</h1>
+        <UncontrolledForm />
+      </div>
+  )
+}
+
+export default App
